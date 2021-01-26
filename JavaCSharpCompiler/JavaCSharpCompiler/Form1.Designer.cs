c86e6c22feca4c0cacd2ad50bac5e5f60124b0db
dragonborn456 <63712441+dragonborn456@users.noreply.github.com>
2021-01-26 15:14:28 +0700

    Add files via upload

---
+++ JavaCSharpCompiler/JavaCSharpCompiler/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Ubuntuconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCmd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCmd
+            // 
+            this.btnCmd.Location = new System.Drawing.Point(42, 18);
+            this.btnCmd.Name = "btnCmd";
+            this.btnCmd.Size = new System.Drawing.Size(183, 174);
+            this.btnCmd.TabIndex = 1;
+            this.btnCmd.Text = "Tạo Image và Container";
+            this.btnCmd.UseVisualStyleBackColor = true;
+            this.btnCmd.Click += new System.EventHandler(this.BtnCmd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 210);
+            this.Controls.Add(this.btnCmd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCmd;
+    }
+}
+
---
+++ JavaCSharpCompiler/JavaCSharpCompiler/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ubuntuconnect
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public string ExecuteCommandSync(object commmand)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo processStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c" + commmand);
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc =
+                   new System.Diagnostics.Process();
+                proc.StartInfo = processStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                return result;
+
+            }
+            catch (Exception objException)
+            {
+                return "";
+            }
+
+        }
+
+        private void BtnCmd_Click(object sender, EventArgs e)
+        {
+            string commmand = "docker build -t onlinecompiler C:/Users/Admin/demo";
+            ExecuteCommandSync(commmand);
+            string commmand1 = "docker run -p 4000:80 --name phong onlinecompiler";
+            ExecuteCommandSync(commmand1);
+            btnCmd.BackColor = Color.Blue;
+        }
+
+
+    }
+}
